8083a88d95afb1e5ecbbac3fc089ff32762cf28c
Рэм Кудусов <rem.kudusov@gmail.com>
2025-04-24 22:41:01 +0300

    Initial commit.

---
+++ Assets/_CrossyBridge/Components/Coloring.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Компонент-пустышка для окрашиваемых частей автомобиля.
+/// </summary>
+public class Coloring : MonoBehaviour
+{
+
+}
